b2ca96460b998b20f487d2a47ee457fdb07a4622
bmindaugas <37580537+bmindaugas@users.noreply.github.com>
2018-06-22 20:16:47 +0300

    trimestras

---
+++ JavaYoutube2/src/com/company/Papildoma.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Papildoma {
+
+  public  Papildoma(String vard){
+    System.out.println("Labas "+vard);
+  }
+
+//  public void zinute(String vardas){
+//    System.out.println("Sveiki "+vardas);
+//  }
+}
